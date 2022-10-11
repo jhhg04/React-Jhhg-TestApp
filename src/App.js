c7f5cf860a4abb2dbfcf5e9e48b955093e5355e9
jhhg0 <jhherrera@ccss.sa.cr>
2022-10-10 20:01:02 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+// min 1:10:00
+import { useEffect, useState } from 'react';
+import { getCountries } from './services/countries';
+
 const App = () => {
-  return <div className='container'>Hello</div>;
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      setCountries(await getCountries());
+    })();
+  }, []);
+
+  const countryHandler = (e) => {
+    const country = e.currentTarget.value;
+    console.log(country);
+  };
+
+  console.log(countries);
+  return (
+    <>
+      <div>
+        <label>Elige un pais</label>
+        <select onChange={countryHandler}>
+          {countries.map((country) => (
+            <option key={country.cca2} value={country.cca2}>
+              {country.name.common}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
+  );
 };
 
 export default App;
